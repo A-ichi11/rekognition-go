f139f86426d0670ed37da9057f3769bef704324a
EikoNakashima <68436861+EikoNakashima@users.noreply.github.com>
2021-09-23 10:29:50 +0900

    test

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/EikoNakashima/rekognition-go.git
 
 go 1.15
+
+require github.com/aws/aws-sdk-go v1.40.45 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,58 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/rekognition"
+)
+
+const imagePath = "image/"
+
+var imageName1 = "gun.png"
+var imageName2 = "human.jpg"
+var imageName3 = "sakura.jpeg"
+var imageName4 = "水着.png"
+
+func main() {
+	// imageNameを変更して実行することで、画像を変更します。
+	// ここ雑です。すいません。
+	file := imagePath + imageName4
+
+	// 画像ファイルを取得
+	imageFile, err := os.Open(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// 最後に画像ファイルを閉じます
+	defer imageFile.Close()
+
+	// 画像ファイルのデータを読み込み
+	bytes, err := ioutil.ReadAll(imageFile)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	// セッション作成
+	sess := session.Must(session.NewSession())
+
+	// Rekognitionクライアントを作成
+	svc := rekognition.New(sess, aws.NewConfig().WithRegion("us-east-1"))
+
+	// パラメータを設定
+	params := &rekognition.DetectModerationLabelsInput{
+		Image: &rekognition.Image{
+			Bytes: bytes,
+		},
+	}
+
+	// DetectModerationLabelsを実行。resultに結果のLabelが入ります。
+	result, err := svc.DetectModerationLabels(params)
+	// 結果を出力
+	fmt.Println(result)
 }
